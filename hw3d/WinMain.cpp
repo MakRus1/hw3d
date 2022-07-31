b0a9d9dc64d12090ec58937f3d347e8d431e1242
MakRus <macs2003148@gmail.com>
2022-07-31 13:53:46 +0300

    Added a functional (closing)

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,5 +1,16 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(69);
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
@@ -8,11 +19,12 @@ int CALLBACK WinMain(
 )
 {
 	const LPCWSTR pClassName = L"hw3dbutts";
+
 	// register window class
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -23,6 +35,7 @@ int CALLBACK WinMain(
 	wc.lpszClassName = pClassName;
 	wc.hIconSm = nullptr;
 	RegisterClassEx( &wc );
+
 	// create window instance
 	HWND hWnd = CreateWindowEx(
 		0, pClassName,
@@ -31,8 +44,25 @@ int CALLBACK WinMain(
 		200, 200, 640, 480,
 		nullptr, nullptr, hInstance, nullptr
 	);
+
 	// show the damn window
 	ShowWindow(hWnd, SW_SHOW);
-	while (true);
-	return 0;
+	
+	// message pump
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 }
