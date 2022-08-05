1ff68f3b47d25537d5173f6ee93e57e4077edb89
MakRus <macs2003148@gmail.com>
2022-08-05 16:21:09 +0300

    Incapsulating window class

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,15 +1,4 @@
-#include <Windows.h>
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	switch (msg)
-	{
-	case WM_CLOSE:
-		PostQuitMessage(69);
-		break;
-	}
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
+#include "Window.h"
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -18,36 +7,8 @@ int CALLBACK WinMain(
 	int nCmdShow
 )
 {
-	const LPCWSTR pClassName = L"hw3dbutts";
+	Window wnd(800, 300, L"Donkey Fart Box");
 
-	// register window class
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = nullptr;
-	RegisterClassEx( &wc );
-
-	// create window instance
-	HWND hWnd = CreateWindowEx(
-		0, pClassName,
-		L"Happy Hard Window",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200, 200, 640, 480,
-		nullptr, nullptr, hInstance, nullptr
-	);
-
-	// show the damn window
-	ShowWindow(hWnd, SW_SHOW);
-	
 	// message pump
 	MSG msg;
 	BOOL gResult;
@@ -61,8 +22,6 @@ int CALLBACK WinMain(
 	{
 		return -1;
 	}
-	else
-	{
-		return msg.wParam;
-	}
+
+	return msg.wParam;
 }
---
+++ hw3d/Window.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "ChiliWIn.h"
+
+class Window
+{
+private:
+	class WindowClass
+	{
+	public:
+		static const LPCWSTR GetName() noexcept;
+		static HINSTANCE GetInstance() noexcept;
+
+	private:
+		WindowClass() noexcept;
+		~WindowClass();
+		WindowClass(const WindowClass&) = delete;
+		WindowClass& operator = (const WindowClass&) = delete;
+		static constexpr const LPCWSTR wndClassName = L"Chili Direct3D Engine Window";
+		static WindowClass wndClass;
+		HINSTANCE hInst;
+	};
+
+public:
+	Window(int width, int height, const LPCWSTR name) noexcept;
+	~Window();
+	Window(const Window&) = delete;
+	Window& operator = (const Window&) = delete;
+
+private:
+	static LRESULT CALLBACK HandleMsgSetup(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+	static LRESULT CALLBACK HandleMsgThunk(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+	LRESULT HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+
+private:
+	int width;
+	int height;
+	HWND hWnd;
+};
