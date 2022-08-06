ba865bc0630b2d9c75790811435a785fe951b12b
MakRus <macs2003148@gmail.com>
2022-08-06 17:13:47 +0300

    Handle mouse wheel events

--- hw3d/Mouse.h
+++ hw3d/Mouse.h
@@ -116,6 +116,7 @@ private:
 	void OnWheelUp(int x, int y) noexcept;
 	void OnWheelDown(int x, int y) noexcept;
 	void TrimBuffer() noexcept;
+	void OnWheelDelta(int x, int y, int delta) noexcept;
 
 private:
 	static constexpr unsigned int bufferSize = 16u;
@@ -124,6 +125,7 @@ private:
 	bool leftIsPressed = false;
 	bool rightIsPressed = false;
 	bool isInWindow = false;
+	int wheelDeltaCarry = 0;
 	std::queue<Event> buffer;
 };
 
